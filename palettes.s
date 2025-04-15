34ccb64966d8053c228093fdaba9553130d9e99f
Mishcatt <mieszkoter@gmail.com>
2025-04-15 22:38:44 +0200

    Split into more files

---
+++ palettes.s
@@ -0,0 +1,12 @@
+palettes:
+    ; Background Palette
+    .byte $0F, $11, $16, $1A
+    .byte $0f, $00, $00, $00
+    .byte $0f, $00, $00, $00
+    .byte $0f, $00, $00, $00
+
+    ; Sprite Palette
+    .byte $0f, $20, $00, $00
+    .byte $0f, $00, $00, $00
+    .byte $0f, $00, $00, $00
+    .byte $0f, $00, $00, $00
---
+++ sprites.s
@@ -0,0 +1,8 @@
+sprites:
+    .byte $00, $00, $00, $00 	; Why do I need these here?
+    .byte $00, $00, $00, $00    ; Ypos, Index, Attributes, Xpos
+    .byte $60, $02, $00, $60
+    .byte $64, $03, $00, $70
+    .byte $68, $04, $00, $80
+    .byte $6C, $05, $00, $90
+    .byte $70, $06, $00, $A0
