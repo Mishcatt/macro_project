88013b0439560c814e7ae1d06735ee9e2bd44548
Mishcatt <mieszkoter@gmail.com>
2025-04-15 23:00:53 +0200

    NMI in separate file

--- main.s
+++ main.s
@@ -94,7 +94,6 @@ vblankwait2:
     bit PPUSTATUS
     bpl vblankwait2
 
-main:
 load_palettes:
     lda PPUSTATUS ; clear w register by reading Status
     lda #$3f
@@ -243,58 +242,7 @@ DoDrawing:
 MusicEngine:
     rts
 
-;--------------------------------------
-; NMI - the NMI handler
-nmi:
-    pha         ; back up registers (important)
-    txa
-    pha
-    tya
-    pha
-
-    @check_dma_flag:
-        lda dmaflag
-        beq @check_draw_flag
-            lda #0      ; do sprite DMA
-            sta OAMADDR   ; conditional via the 'dmaflag' flag
-            lda #>OAM
-            sta OAMDMA
-            dec dmaflag
-
-    @check_draw_flag:
-        lda drawflag          ; do other PPU drawing (NT/Palette/whathaveyou)
-        beq @check_ppu_flag ; conditional via the 'drawflag' flag
-            bit PPUSTATUS         ; clear VBl flag, reset PPUSCROLL/PPUADDR toggle
-            jsr DoDrawing     ; draw the stuff from the drawing buffer
-            dec drawflag
-
-    @check_ppu_flag:
-        lda ppuflag
-        beq @music_handler
-            lda softPPUMASK   ; copy buffered PPUCTRL/PPUMASK (conditional via ppuflag)
-            sta PPUMASK
-            lda softPPUCTRL
-            sta PPUCTRL
-            bit PPUSTATUS
-            lda xscroll    ; set X/Y scroll (conditional via ppuflag)
-            sta PPUSCROLL
-            lda yscroll
-            sta PPUSCROLL
-            dec ppuflag
-
-    @music_handler:
-        jsr MusicEngine
-
-    lda #1         
-    sta nmi_flag   ; set the nmi_flag flag so that main_loop will continue
-
-    pla            ; restore regs and exit
-    tay
-    pla
-    tax
-    pla
-    rti
-
+.include "nmi.s"
 .include "sprites.s"
 .include "palettes.s"
 .include "chars.s"
---
+++ nmi.s
@@ -0,0 +1,51 @@
+;--------------------------------------
+; NMI - the NMI handler
+nmi:
+    pha         ; back up registers (important)
+    txa
+    pha
+    tya
+    pha
+
+    @check_dma_flag:
+        lda dmaflag
+        beq @check_draw_flag
+            lda #0      ; do sprite DMA
+            sta OAMADDR   ; conditional via the 'dmaflag' flag
+            lda #>OAM
+            sta OAMDMA
+            dec dmaflag
+
+    @check_draw_flag:
+        lda drawflag          ; do other PPU drawing (NT/Palette/whathaveyou)
+        beq @check_ppu_flag ; conditional via the 'drawflag' flag
+            bit PPUSTATUS         ; clear VBl flag, reset PPUSCROLL/PPUADDR toggle
+            jsr DoDrawing     ; draw the stuff from the drawing buffer
+            dec drawflag
+
+    @check_ppu_flag:
+        lda ppuflag
+        beq @music_handler
+            lda softPPUMASK   ; copy buffered PPUCTRL/PPUMASK (conditional via ppuflag)
+            sta PPUMASK
+            lda softPPUCTRL
+            sta PPUCTRL
+            bit PPUSTATUS
+            lda xscroll    ; set X/Y scroll (conditional via ppuflag)
+            sta PPUSCROLL
+            lda yscroll
+            sta PPUSCROLL
+            dec ppuflag
+
+    @music_handler:
+        jsr MusicEngine
+
+    lda #1         
+    sta nmi_flag   ; set the nmi_flag flag so that main_loop will continue
+
+    pla            ; restore regs and exit
+    tay
+    pla
+    tax
+    pla
+    rti
