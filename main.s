025d2f62cff08420eb1b0fef6c35d7d5aae55d35
Mishcatt <mieszkoter@gmail.com>
2025-04-19 23:35:04 +0200

    Proper background drawing

--- main.s
+++ main.s
@@ -35,7 +35,9 @@ drawingLoop2: .res 1
 
 temp1: .res 1
 temp2: .res 1
+temp2a: .res 1
 temp3: .res 1
+temp3a: .res 1
 temp4: .res 1
 
 ;leftRenderColumn: .res 30
@@ -249,7 +251,7 @@ readjoyx:           ; X register = 0 for controller 1, 1 for controller 2
     bcc @loop
     rts
 
-DoDrawing2:
+DoDrawing:
     lda #%10000100  ; Enable NMI and vertical increment
     sta PPUCTRL
     bit PPUSTATUS   ; clear w register by reading Status
@@ -369,206 +371,94 @@ PrepareDrawingTest:
     rts
 
 PrepareDrawing:
-    lda currentMapColumn 
-    tax             ; copy map column number to X
-    asl a           ; map columns are 16px, PPU columns are 8px, so x2
-    sta tempMapAddress
+    ldy #0
+    sty temp2a
+    lda currentRenderColumn
+    asl
+    sta temp2
 
-    lda map, x      ; load column type number to A
-    asl a
-    asl a
-    asl a
-    asl a           ; x16 (column size)
-    sta tempColumnAddress
+    @drawing3:
+        lda temp2
+        ora temp2a ; add map offset
+        tax
+        ;inc temp2a ; 0 1
+        lda map, x
+        asl
+        asl
+        asl
+        asl
+        sta temp3
+        lda #0
+        sta temp3a
 
-    lda #0
-    sta tempDrawAddressOffset
-    @loop1:
-        ldy #0
-        @loop2:
-            ldx tempColumnAddress   ; load rendering column offset to X
-            inc tempColumnAddress   ; increment the offset
-            lda columns, x          ; load block type number to A
-            asl a 
-            asl a           ; x4 (block size)
-            tax             ; copy char type address to X
-            lda blocks, x   ; load char number to A
-            ldx tempDrawAddressOffset
-            sta DRAWBUFFER, x
-            inx
-            sta DRAWBUFFER, x
-            stx tempDrawAddressOffset
-            iny
-            cpy #30         ; last row
-            bcc @loop2
-        lda tempDrawAddressOffset
-        cmp #120
-        bcc @loop1
-
-    lda currentMapColumn
-    and #1
-    bne @skipColorRender
-    jmp @skipColorRender
-        lda map, x      ; load column type number to A
-        asl a
-        asl a
-        asl a
-        asl a           ; x16 (column size)
-        sta tempColumnAddress
-        ldy #0
-
-        @loop3:
-            lda #$23
-            sta PPUADDR
-            lda currentMapColumn
-            sta tempColorAddress
-            lsr tempColorAddress
-            tya
-            asl a
-            asl a
-            asl a
+        @drawing2A:
+            lda temp3
             clc
-            adc #$C0
-            adc tempColorAddress
-            sta PPUADDR
-            ldx tempColumnAddress
-            inc tempColumnAddress
-            inc tempColumnAddress
-            lda columns, x      ; load block type number to A
+            adc temp3a ; add column offset
             tax
-            lda blockColors, x  ; load block color number to X
-            ; lda #1
-            sta PPUDATA
-            iny
-            cpy #7         ; last row (should be 7, too many cycles)
-            bcc @loop3
-    @skipColorRender:
+            inc temp3a ; 0 ... 14
+            lda columns, x
+            asl
+            asl ; x4
+            sta temp4
 
-    ldx currentMapColumn
-    inx         ; next column
-    cpx #16    ; over last column?
-    bcc :+
-        ldx #0  ; back to column 0
-    :
-    stx currentMapColumn
+            @drawing1A:
+                ldx temp4  ; 0 1
+                lda blocks, x
+                sta DRAWBUFFER, y
+                iny
+                inx
+                lda blocks, x
+                sta DRAWBUFFER, y
+                iny
 
-    rts
+            lda temp3a
+            cmp #15
+            bcc @drawing2A
 
-DoDrawing:
-    lda #%10000100  ; Enable NMI and vertical increment
-    sta PPUCTRL
-    lda PPUSTATUS   ; clear w register by reading Status
-    lda #$20        ; nametable 0, TODO: nametable 1
-    sta PPUADDR
-    lda currentMapColumn 
-    tax             ; copy map column number to X
-    asl a           ; map columns are 16px, PPU columns are 8px, so x2
-    sta PPUADDR
+        lda temp2
+        ora temp2a ; add map offset
+        tax
+        inc temp2a ; 0 1
+        lda map, x
+        asl
+        asl
+        asl
+        asl
+        sta temp3
+        lda #0
+        sta temp3a
 
-    lda map, x      ; load column type number to A
-    asl a
-    asl a
-    asl a
-    asl a           ; x16 (column size)
-    sta tempColumnAddress
-    ldy #0
-
-    @loop1:
-        ldx tempColumnAddress   ; load rendering column offset to X
-        inc tempColumnAddress   ; increment the offset
-        lda columns, x  ; load block type number to A
-        asl a 
-        asl a           ; x4 (block size)
-        tax             ; copy char type address to X
-        lda blocks, x   ; load char number to A
-        sta PPUDATA
-        inx
-        lda blocks, x   ; load char number to A
-        sta PPUDATA
-        iny
-        cpy #15         ; last row
-        bcc @loop1
-
-    lda #$20        ; nametable 0, TODO: nametable 1
-    sta PPUADDR
-    lda currentMapColumn 
-    tax             ; copy map column number to X
-    asl a           ; map columns are 16px, PPU columns are 8px, so x2
-    ora #1          ; add 1 for second column
-    sta PPUADDR
-
-    lda map, x      ; load column type number to A
-    asl a
-    asl a
-    asl a
-    asl a           ; x16 (column size)
-    sta tempColumnAddress
-    ldy #0
-
-    @loop2:
-        ldx tempColumnAddress   ; load rendering column offset to X
-        inc tempColumnAddress   ; increment the offset
-        lda columns, x  ; load block type number to A
-        asl a 
-        asl a           ; x4 (block size)
-        tax             ; copy char type address to X
-        lda blocks, x   ; load char number to A
-        sta PPUDATA
-        inx
-        lda blocks, x   ; load char number to A
-        sta PPUDATA
-        iny
-        cpy #15         ; last row
-        bcc @loop2
-
-    lda #%10000000  ; Enable NMI and horizontal increment
-    sta PPUCTRL
-
-    lda currentMapColumn
-    and #1
-    bne @skipColorRender
-        lda map, x      ; load column type number to A
-        asl a
-        asl a
-        asl a
-        asl a           ; x16 (column size)
-        sta tempColumnAddress
-        ldy #0
-
-        @loop3:
-            lda #$23
-            sta PPUADDR
-            lda currentMapColumn
-            sta tempColorAddress
-            lsr tempColorAddress
-            tya
-            asl a
-            asl a
-            asl a
-            adc #$C0
-            adc tempColorAddress
-            sta PPUADDR
-            ldx tempColumnAddress
-            inc tempColumnAddress
-            inc tempColumnAddress
-            lda columns, x      ; load block type number to A
+        @drawing2B:
+            lda temp3
+            clc
+            adc temp3a ; add column offset
             tax
-            lda blockColors, x  ; load block color number to X
-            ; lda #1
-            sta PPUDATA
-            iny
-            cpy #5         ; last row (should be 7, too many cycles)
-            bcc @loop3
-    @skipColorRender:
+            inc temp3a ; 0 ... 14
+            lda columns, x
+            asl
+            asl ; x4
+            sta temp4
 
-    ldx currentMapColumn
-    inx         ; next column
-    cpx #$10    ; over last column?
-    bcc :+
-        ldx #0  ; back to column 0
-    :
-    stx currentMapColumn
-
+            @drawing1B:
+                ldx temp4   ; 2 3
+                inx         ;
+                inx         ;
+                lda blocks, x
+                sta DRAWBUFFER, y
+                iny
+                inx
+                lda blocks, x
+                sta DRAWBUFFER, y
+                iny
+            
+            lda temp3a
+            cmp #15
+            bcc @drawing2B
+        
+        lda temp2a
+        cmp #2
+        bcc @drawing3
     rts
 
 MusicEngine:
