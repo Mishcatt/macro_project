a47603a6f137fb43b3485fb85d84382459c57cb9
Mishcat <mieszkoter@gmail.com>
2025-06-29 22:00:03 +0200

    Famicon17 sprite text

--- sprites.s
+++ sprites.s
@@ -1,17 +1,17 @@
 sprites:
-    .byte $CF, $00, $01, $F8 	; Sprite 0
+    .byte $CF, $00, $00, $F8 	; Sprite 0
     .byte $00, $00, $00, $00    ; Ypos, Index, Attributes, Xpos
     .byte $60, $01, $00, $60
     .byte $64, $02, $00, $70
     .byte $68, $03, $02, $80
     .byte $6C, $04, $03, $90
     .byte 0, $05, $01, 124
-    .byte $D6, $10, $00, $10 ; F
-    .byte $D7, $0B, $00, $18 ; A
-    .byte $D8, $17, $00, $20 ; M
-    .byte $D9, $13, $00, $26 ; I
-    .byte $DA, $0D, $00, $2C ; C
-    .byte $DB, $19, $00, $34 ; O
-    .byte $DC, $18, $00, $3C ; N
-    .byte $DE, $02, $00, $46 ; 1
-    .byte $DF, $08, $00, $4C ; 7
+    .byte $D6, $06, $00, $10 ; F
+    .byte $D7, $07, $00, $18 ; A
+    .byte $D8, $08, $00, $20 ; M
+    .byte $D9, $09, $00, $26 ; I
+    .byte $DA, $0A, $00, $2C ; C
+    .byte $DB, $0B, $00, $34 ; O
+    .byte $DC, $0C, $00, $3C ; N
+    .byte $DE, $0D, $00, $46 ; 1
+    .byte $DF, $0E, $00, $4C ; 7
