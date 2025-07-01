8ed4630fa2f681c50a0c7fc5ea612f95d6767166
Mishcat <mieszkoter@gmail.com>
2025-07-01 22:49:25 +0200

    Moved joypad constants

--- const.s
+++ const.s
@@ -23,6 +23,15 @@ StatusbarOffsetStompBar := 45
 StatusbarOffsetSize := 21
 StatusbarOffsetSizeBar := 53
 
+BUTTON_A      = 1 << 7
+BUTTON_B      = 1 << 6
+BUTTON_SELECT = 1 << 5
+BUTTON_START  = 1 << 4
+BUTTON_UP     = 1 << 3
+BUTTON_DOWN   = 1 << 2
+BUTTON_LEFT   = 1 << 1
+BUTTON_RIGHT  = 1 << 0
+
 .enum Sprites
     Sprite0y = 0
     Sprite0Index
